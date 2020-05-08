d90be66befed1dc3bb092ee730d06d44703f8bd5
yutveg <kodi.0n31l@gmail.com>
2020-05-08 08:44:47 -0700

    ex2 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,17 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-
+    look_up = {}
+    for ticket in tickets:
+        look_up[ticket.source] = ticket.destination
+    
+    route = []
+    first_destination = look_up["NONE"]
+    route.append(first_destination)
+    # cur_node = first destination
+    cur_node = first_destination
+    while cur_node != "NONE":
+        cur_node = look_up[cur_node]
+        route.append(cur_node)
+    
     return route
