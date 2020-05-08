0225f157f8f736a7a95c23defb31c261da953824
yutveg <kodi.0n31l@gmail.com>
2020-05-08 08:29:31 -0700

    ex1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,20 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    look_up = {}
+    for index, weight in enumerate(weights):
+        print(weight, index)
+        look_up[weight] = index
+    
+    
 
+    for index, weight in enumerate(weights):
+        print(f"limit is {limit}, weight is {weight}, need a {limit - weight}")
+        if limit - weight in look_up:
+            if limit - weight > weight:
+                return (look_up[limit - weight], index)
+            else:
+                return (index, look_up[limit - weight])
+
+            
     return None
