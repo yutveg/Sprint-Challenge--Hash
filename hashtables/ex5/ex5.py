b791d415fbbf25b0cad1612d8acdbb02bf7df1bf
yutveg <kodi.0n31l@gmail.com>
2020-05-08 09:09:42 -0700

    ex5 done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -3,7 +3,17 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
+    result = []
+    look_up = {}
+    for path in queries:
+        look_up[path] = 1
+    
+    for fullpath in files:
+        new_tuple = tuple(fullpath.split('/'))
+        if new_tuple[-1] in look_up:
+            result.append('/'.join(new_tuple))
 
+    print(result)
     return result
 
 
